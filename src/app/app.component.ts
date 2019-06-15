795b2e54fcc1900331d15264a7ce74b0ca0be63c
rhythm010 <rhythm.khanna8@gmail.com>
2019-06-15 23:30:37 +0530

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { AngularFirestore } from 'angularfire2/firestore';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
   name = 'MyReply.in';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, FlexLayoutModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
